78026313124564bda8985040cff9f1250ae4d7cd
delwerHossain <delwerhossain006@gmail.com>
2023-03-21 14:37:12 +0000

    add react

--- test-project/src/App.js
+++ test-project/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+const teachers = { name: 'rakib', title: 'mr', job: 'teacher' }
+const styleT = {color: 'red'} 
 function App() {
   return (
     <div className="App">
-     <Post></Post>
+     <Post name={teachers.name} ></Post>
     </div>
   );
 }
@@ -12,7 +13,7 @@ function App() {
 const Post = () => { 
   return (
     <div>
-      <h1>this is heading</h1>
+      <h1 style= {styleT} >name : {teachers.name}</h1> 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quibusdam itaque temporibus autem harum assumenda facere officiis a perspiciatis error! Sit laudantium alias facere illo est amet earum nemo atque, dignissimos, dolor voluptates porro ipsam quia ipsa eum rerum adipisci.</p>
     </div>
   );
