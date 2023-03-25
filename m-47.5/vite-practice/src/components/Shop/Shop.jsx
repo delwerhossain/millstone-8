626fa618bfd0167b2bee9ad16f386f1f467699a3
delwerHossain <delwerhossain006@gmail.com>
2023-03-25 13:16:41 +0000

    new projcet

---
+++ m-47.5/vite-practice/src/components/Shop/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>product name :</h2>       
+            <p> details : </p>
+            <h3>price : </h3>     
+        </div>
+    );
+};
+
+export default Shop;
---
+++ m-49/shopping-cart/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <h1 className='text-red-500'> Shopping Cart </h1>
+    </div>
+  )
+}   
+
+export default App
